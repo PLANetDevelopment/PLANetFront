98d34a79f4f438558ea2556e089508c4cef9e11c
DESKTOP-NR7CBTO\user <listenyoon@gmail.com>
2022-01-22 14:58:28 +0900

    add basic layout

--- planet-front/src/App.js
+++ planet-front/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import Calendar from './pages/Calendar/Calendar';
+import Statistics from './pages/Statistics/Statistics';
+import Diary from './pages/Diary/Diary';
+import News from './pages/News/News';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/calendar" element={<Calendar/>}></Route>
+        <Route path="/statistics" element={<Statistics/>}></Route>
+        <Route path="/diary" element={<Diary/>}></Route>
+        <Route path="/news" element={<News/>}></Route>
+    </Routes>
   );
 }
 

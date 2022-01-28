6b602a6e1a449c1b9bfec8faca6fd29d22bd189d
zivvon <brenna9981@gmail.com>
2022-01-28 16:21:32 +0900

    날짜 입력 이벤트 개발 시작

--- planet-front/src/pages/Floating/FloatingDate.js
+++ planet-front/src/pages/Floating/FloatingDate.js
@@ -3,6 +3,7 @@ import IncomeStyle from './Float.module.css';
 import TopNav from '../../components/FloatingPart/TopNav';
 import Dashboard from '../../components/FloatingPart/Dashboard';
 import IncomeDate from '../../components/FloatingPart/IncomeDate';
+import BottomBtn from '../../components/FloatingPart/BottomBtn';
 
 //Content
 class Content extends Component {
@@ -25,6 +26,8 @@ function FloatingDate() {
 
       <Content title="언제 받으셨나요?"></Content>
       <h1><IncomeDate></IncomeDate></h1>
+
+      <BottomBtn></BottomBtn>
 
     </div>
   );
